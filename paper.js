fd814ae1ba8745cd1db2a0f84f1c74bc5e708116
SHLOKTIWARI <66375969+SHLOKTIWARI@users.noreply.github.com>
2020-07-12 11:26:05 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,41 @@
+class paper{
+    constructor(x,y,r){
+
+      
+
+        var options={
+           isStatic:false,
+            restitution:0.3,
+            density:1.2,
+            friction:0.5
+    }
+    this.r=r;
+    this.x=x;
+    this.y=y;
+
+    //this.image = loadImage("paper.png");
+    
+
+    this.body = Bodies.circle(this.x,this.y,this.r/2, options);
+  
+
+
+     World.add(world, this.body);
+  }
+  display(){
+     var paperpos=this.body.position
+     push();
+    translate(paperpos.x,paperpos.y);
+    
+    fill("purple")
+    ellipseMode(CENTER);
+    ellipse( 0, 0, this.r, this.r);
+    pop();
+  }
+}
+
+
+
+
+
+
